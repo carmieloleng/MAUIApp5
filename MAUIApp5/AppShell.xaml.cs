99521cdb207c1ed835854064a5a06d9b9ca88901
Fhranz Joseph Abella Benosa <97322613+decaxcs@users.noreply.github.com>
2023-10-16 09:22:54 +0800

    submission

---
+++ MAUIApp5/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUIApp5;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MAUIApp5/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MAUIApp5;
+
+public partial class MainPage : ContentPage
+{
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	public void OnVerticalStart(object sender, EventArgs e)
+	{
+		target.VerticalOptions = LayoutOptions.Start;
+	}
+
+    public void OnVerticalCenter(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.Center;
+    }
+    public void OnVerticalEnd(object sender, EventArgs e)
+    {
+        target.VerticalOptions = LayoutOptions.End;
+    }
+
+    public void OnHorizontalStart(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.Start;
+    }
+    public void OnHorizontalCenter(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.Center;
+    }
+    public void OnHorizontalEnd(object sender, EventArgs e)
+    {
+        target.HorizontalOptions = LayoutOptions.End;
+    }
+}
+
